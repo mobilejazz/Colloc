dac54e937188b537e086c475f9b021212f487cd0
Diego Freniche Brito <diego@freniche.com>
2016-10-27 11:56:11 +0200

    Added Swift proof-of-concept sample

---
+++ Sample Projects/iOS Swift Sample/iOS Swift Sample/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  iOS Swift Sample
+//
+//  Created by Diego Freniche Brito on 27/10/16.
+//  Copyright © 2016 Diego Freniche Brito. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
