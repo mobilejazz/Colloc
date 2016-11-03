49c347a7866e104aa96b42340c1de95d271c63e8
Diego Freniche Brito <diego@freniche.com>
2016-11-03 10:56:43 +0100

    Added target to iOS Sample integrating Colloc

--- Sample Projects/iOS Sample/iOS Sample/Languages/Localization.h
+++ Sample Projects/iOS Sample/iOS Sample/Languages/Localization.h
@@ -1,4 +1,4 @@
-/**	This is an automatically generated file	Please don't modify it.	If you need to change some text please do so at	sampleGDocFile.tsv	*/#define tr_generic_ok NSLocalizedString(@"tr_generic_ok", nil)
+/**	This is an automatically generated file	Please don't modify it.	If you need to change some text please do so at	/Users/dfreniche/SyncFiles/MobileJazz/MJ-OpenSource/Colloc/Sample Projects/iOS Sample/iOS Sample/Languages/sampleGDocFile.tsv	*/#define tr_generic_ok NSLocalizedString(@"tr_generic_ok", nil)
 #define tr_generic_dismiss NSLocalizedString(@"tr_generic_dismiss", nil)
 #define tr_generic_cancel NSLocalizedString(@"tr_generic_cancel", nil)
 #define tr_generic_done NSLocalizedString(@"tr_generic_done", nil)
