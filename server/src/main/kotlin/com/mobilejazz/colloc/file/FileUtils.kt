f3aebbaf0098869da617e212451b971058606319
mecofarid <mecofarid@gmail.com>
2022-06-09 15:34:50 +0100

    Added file/directory generation and directory zipping

---
+++ server/src/main/kotlin/com/mobilejazz/colloc/file/FileUtils.kt
@@ -0,0 +1,33 @@
+package com.mobilejazz.colloc.file
+
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+object FileUtils {
+    fun createFile(directory: String, fileName: String) {
+        // Create directory if not doesn't exist
+        createDirectory(directory)
+        File(directory, fileName).createNewFile()
+    }
+
+    fun createDirectory(directory: String) =
+            File(directory).mkdirs()
+
+    fun generateZip(fileToZip: File, zipFile: File) {
+        ZipOutputStream(BufferedOutputStream(FileOutputStream(zipFile))).use { zos ->
+            fileToZip.walkTopDown().forEach { file ->
+                val zipEntry = file.toZipEntry(fileToZip)
+                zos.putNextEntry(zipEntry)
+                if (file.isFile) {
+                    file.inputStream().copyTo(zos)
+                }
+            }
+        }
+    }
+
+    private fun File.toZipEntry(sourceFile: File): ZipEntry {
+        val zipFileName = this.absolutePath.removePrefix(sourceFile.absolutePath).removePrefix("/")
+        return ZipEntry( "$zipFileName${(if (this.isDirectory) "/" else "" )}")
+    }
+}
