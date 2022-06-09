f6fb129d215f355a0c1bf439fbb13683c22f3e55
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2022-06-09 18:01:07 +0100

    wip

--- server/src/main/kotlin/com/mobilejazz/colloc/csv/ParseCsvInteractor.kt
+++ server/src/main/kotlin/com/mobilejazz/colloc/csv/ParseCsvInteractor.kt
@@ -1,25 +1,35 @@
 package com.mobilejazz.colloc.csv
 
-import com.opencsv.bean.CsvToBeanBuilder
+import com.opencsv.CSVReader
+import java.io.File
 import java.io.FileReader
 
+
 class ParseCsvInteractor {
-    operator fun invoke(filePath: String): Map<String, Map<String, String>>{
-        val translationItemList =  CsvToBeanBuilder<TranslationItem>(FileReader(filePath))
-                .withType(TranslationItem::class.java)
-                .build()
-                .parse()
+    operator fun invoke(file: File): Map<String, Map<String, String>>{
+        val reader = CSVReader(FileReader(file))
+        var nextLine: Array<String>
+        while (reader.readNext().also { nextLine = it } != null) {
+            // nextLine[] is an array of values from the line
+            println(nextLine[0] + nextLine[1] + "etc...")
+        }
+
+        return emptyMap()
+//        val translationItemList =  CsvToBeanBuilder<TranslationItem>(FileReader(filePath))
+//                .withType(TranslationItem::class.java)
+//                .build()
+//                .parse()
     }
 
-    private fun List<TranslationItem>.map(): Map<String, Map<String, String>> {
-        val languageMap = mutableMapOf<String, Map<String, String>>()
-        forEach {
-            if (it.key.isAlphanumeric()) {
-                val keyTranslationMap = mutableMapOf<String, String>()
-                languageMap.getOrPut(, keyTranslationMap)
-            }
-        }
-    }
+//    private fun List<TranslationItem>.map(): Map<String, Map<String, String>> {
+//        val languageMap = mutableMapOf<String, Map<String, String>>()
+////        forEach {
+////            if (it.key.isAlphanumeric()) {
+////                val keyTranslationMap = mutableMapOf<String, String>()
+////                languageMap.getOrPut(, keyTranslationMap)
+////            }
+////        }
+//    }
 
     private val alphaNumericRegex = "^[a-zA-Z0-9_]*\$".toRegex()
     private fun String.isAlphanumeric() = matches(alphaNumericRegex)
---
+++ server/src/test/kotlin/com/mobilejazz/colloc/ParseCsvInteractorTest.kt
@@ -0,0 +1,18 @@
+package com.mobilejazz.colloc
+
+import com.mobilejazz.colloc.csv.ParseCsvInteractor
+import com.mobilejazz.colloc.domain.interactor.DownloadFileInteractor
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ParseCsvInteractorTest {
+
+  @Test
+  fun `should success`() = runBlocking {
+    val interactor = ParseCsvInteractor()
+    val download = DownloadFileInteractor()
+    download("")
+    assertEquals(true, true)
+  }
+}
