2463c5693415b5095d5b6d9c87b01bede5985a6d
Jordi Giménez <jordi@mobilejazz.com>
2022-06-09 17:25:36 +0100

    drone

--- server/bin/deploy.sh
+++ server/bin/deploy.sh
@@ -2,7 +2,7 @@
 # push the image
 docker login --username "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD" registry.mobilejazz.com || exit
 DOCKER_IMAGE_TAG="$(git rev-parse --short HEAD)"
-docker push registry.mobilejazz.com/harmony/documentation:"$DOCKER_IMAGE_TAG"
+docker push registry.mobilejazz.com/colloc/server:"$DOCKER_IMAGE_TAG"
 
 # copy docker-compose.yml, set the version in the .env file and docker-compose up
 scp docker-compose.prod.yml "$SERVER_USER"@"$SERVER_ADDRESS":docker-compose.yml
