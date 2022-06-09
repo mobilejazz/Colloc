b582883a26af0a0f6f4c847a5914ee54e2d58d49
Alex-DA <Alex-DA@users.noreply.github.com>
2022-06-09 13:09:29 +0200

    first end point

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- server/src/main/kotlin/com/mobilejazz/colloc/CollocApplication.kt
+++ server/src/main/kotlin/com/mobilejazz/colloc/CollocApplication.kt
@@ -2,10 +2,20 @@ package com.mobilejazz.colloc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
-class CollocApplication
+@RestController
+class CollocApplication {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
+        return String.format("Hello %s!", name)
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<CollocApplication>(*args)
+    runApplication<CollocApplication>(*args)
 }
