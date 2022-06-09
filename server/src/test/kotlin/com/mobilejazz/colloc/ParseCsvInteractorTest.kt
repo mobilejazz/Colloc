08d28f40c1a805ac361c3a741deebe306b2f9071
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2022-06-09 18:09:30 +0100

    wip

--- server/src/test/kotlin/com/mobilejazz/colloc/ParseCsvInteractorTest.kt
+++ server/src/test/kotlin/com/mobilejazz/colloc/ParseCsvInteractorTest.kt
@@ -5,6 +5,7 @@ import com.mobilejazz.colloc.domain.interactor.DownloadFileInteractor
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.net.URL
 
 class ParseCsvInteractorTest {
 
@@ -12,7 +13,11 @@ class ParseCsvInteractorTest {
   fun `should success`() = runBlocking {
     val interactor = ParseCsvInteractor()
     val download = DownloadFileInteractor()
-    download("")
-    assertEquals(true, true)
+    val url = URL("https://docs.google.com/a/mobilejazz.com/spreadsheets/d/1FYWbBhV_dtlSVOTrhdO2Bd6e6gMhZ5_1iklL-QrkM2o/export?format=tsv&id=1FYWbBhV_dtlSVOTrhdO2Bd6e6gMhZ5_1iklL-QrkM2o")
+    val file = download(url = url, filename = "output")
+
+//    println(file.length())
+//    interactor(file)
+    assertEquals(file.length() > 0, true)
   }
 }
