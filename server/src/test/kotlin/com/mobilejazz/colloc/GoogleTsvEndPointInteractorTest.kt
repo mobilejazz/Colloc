da4d2f498b49c639c4d327cc59202390a6978b38
Alex-DA <Alex-DA@users.noreply.github.com>
2022-06-09 18:59:26 +0200

    fix tests

--- server/src/test/kotlin/com/mobilejazz/colloc/GoogleTsvEndPointInteractorTest.kt
+++ server/src/test/kotlin/com/mobilejazz/colloc/GoogleTsvEndPointInteractorTest.kt
@@ -5,39 +5,43 @@ import com.mobilejazz.colloc.domain.model.Platform
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.io.File
 
 class GoogleTsvEndPointInteractorTest {
     @Test
     fun `no link returns null`() {
         runBlocking {
-            val result = (GoogleTsvEndPointInteractor())("", listOf(Platform.IOS))
-            assertEquals(result, null)
+            assertThrows<GoogleTsvEndPointInteractor.Error.InvalidURLException> {
+                (GoogleTsvEndPointInteractor())("", listOf(Platform.IOS))
+            }
         }
     }
 
     @Test
     fun `incorrect link returns null`() {
         runBlocking {
-            val result = (GoogleTsvEndPointInteractor())("some random string", listOf(Platform.IOS))
-            assertEquals(result, null)
+            assertThrows<GoogleTsvEndPointInteractor.Error.InvalidURLException> {
+                (GoogleTsvEndPointInteractor())("some random string", listOf(Platform.IOS))
+            }
         }
     }
 
     @Test
     fun `no google docs link returns null`() {
         runBlocking {
-            val result = (GoogleTsvEndPointInteractor())("https://www.google.com", listOf(Platform.IOS))
-            assertEquals(result, null)
+            assertThrows<GoogleTsvEndPointInteractor.Error.InvalidURLException> {
+                (GoogleTsvEndPointInteractor())("https://www.google.com", listOf(Platform.IOS))
+            }
         }
     }
 
     @Test
     fun `no google spreadsheets link returns null`() {
         runBlocking {
-            val result =
+            assertThrows<GoogleTsvEndPointInteractor.Error.InvalidURLException> {
                 (GoogleTsvEndPointInteractor())("https://docs.google.com/a/somerandomstring", listOf(Platform.IOS))
-            assertEquals(result, null)
+            }
         }
     }
 
