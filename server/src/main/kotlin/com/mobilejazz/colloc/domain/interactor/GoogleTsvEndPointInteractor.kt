777494bea5367f92a4892175914202a8052083c5
Alex-DA <Alex-DA@users.noreply.github.com>
2022-06-09 16:13:53 +0200

    complete workflow

--- server/src/main/kotlin/com/mobilejazz/colloc/domain/interactor/GoogleTsvEndPointInteractor.kt
+++ server/src/main/kotlin/com/mobilejazz/colloc/domain/interactor/GoogleTsvEndPointInteractor.kt
@@ -1,9 +1,73 @@
 package com.mobilejazz.colloc.domain.interactor
 
-class GoogleTsvEndPointInteractor {
-    operator fun invoke(link: String?): Int {
-        val googleLinkValidated = link?.let { it } ?: "https://docs.google.com/a/mobilejazz.com/spreadsheets/d/1FYWbBhV_dtlSVOTrhdO2Bd6e6gMhZ5_1iklL-QrkM2o/export?format=tsv&id=1FYWbBhV_dtlSVOTrhdO2Bd6e6gMhZ5_1iklL-QrkM2o"
+import java.io.File
+import java.net.MalformedURLException
+import java.net.URL
 
-        return 1;
+class GoogleTsvEndPointInteractor {
+    operator fun invoke(link: String?): File? {
+        val validatedLinkOrNull = isLinkValid(link)
+
+        if (validatedLinkOrNull === null) {
+            return null
+        }
+
+        val tsv = downloadTsv(validatedLinkOrNull)
+        val tempFolder = generateTempFolder()
+
+        generateJson(tsv, tempFolder)
+        val zip = compressFolder(tempFolder)
+        deleteTempFolder(tempFolder)
+
+        return zip
+    }
+
+    private fun isLinkValid(link: String?): URL? {
+        if (link === null) {
+            return null
+        }
+
+        try {
+            val url = URL(link)
+            url.toURI()
+            return url
+        } catch (e: MalformedURLException) {
+            return null
+        }
+    }
+
+    /**
+     * @todo
+     */
+    private fun generateTempFolder(): String {
+        return ""
+    }
+
+    /**
+     * @todo
+     */
+    private fun downloadTsv(link: URL): File {
+        return File("")
+    }
+
+    /**
+     * @todo
+     */
+    private fun generateJson(tsv: File, tempFolder: String): Boolean {
+        return true
+    }
+
+    /**
+     * @todo
+     */
+    private fun compressFolder(tempFolder: String): File {
+        return File("")
+    }
+
+    /**
+     * @todo
+     */
+    private fun deleteTempFolder(tempFolder: String): Boolean {
+        return true
     }
 }
--- server/src/test/kotlin/com/mobilejazz/colloc/GoogleTsvEndPointInteractorTest.kt
+++ server/src/test/kotlin/com/mobilejazz/colloc/GoogleTsvEndPointInteractorTest.kt
@@ -3,11 +3,19 @@ package com.mobilejazz.colloc
 import com.mobilejazz.colloc.domain.interactor.GoogleTsvEndPointInteractor
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class GoogleTsvEndPointInteractorTest {
     @Test
-    fun `should class be created correctly`() {
-        val result  = (GoogleTsvEndPointInteractor())("slkdkfjs")
-        assertEquals(result, 1)
+    fun `no link provided returns null`() {
+        val result = (GoogleTsvEndPointInteractor())("")
+        assertEquals(result, null)
+    }
+
+    @Test
+    fun `correct link generates a file`() {
+        val link = "https://docs.google.com/a/mobilejazz.com/spreadsheets/d/1FYWbBhV_dtlSVOTrhdO2Bd6e6gMhZ5_1iklL-QrkM2o/export?format=tsv&id=1FYWbBhV_dtlSVOTrhdO2Bd6e6gMhZ5_1iklL-QrkM2o"
+        val result = (GoogleTsvEndPointInteractor())(link)
+        assert(result is File)
     }
 }
