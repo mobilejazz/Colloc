aa3aad32c8976e4feb682ad7c2a5da3886f6b071
Alex-DA <Alex-DA@users.noreply.github.com>
2022-06-09 16:35:07 +0200

    wip

--- server/src/main/kotlin/com/mobilejazz/colloc/CollocApplication.kt
+++ server/src/main/kotlin/com/mobilejazz/colloc/CollocApplication.kt
@@ -4,6 +4,7 @@ import com.mobilejazz.colloc.domain.interactor.DownloadFileInteractor
 import com.mobilejazz.colloc.domain.interactor.GoogleTsvEndPointInteractor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
