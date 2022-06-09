f2f7236170d1450acb85b26c244f0dc36335f741
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2022-06-09 11:23:25 +0100

    feat: initial new new colloc

---
+++ server/src/main/kotlin/com/mobilejazz/colloc/CollocApplication.kt
@@ -0,0 +1,11 @@
+package com.mobilejazz.colloc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollocApplication
+
+fun main(args: Array<String>) {
+	runApplication<CollocApplication>(*args)
+}
